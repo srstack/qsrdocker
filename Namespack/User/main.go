7f79c3ef24cd42cc016869c0d094840c8021029a
srstack <39378935+srstack@users.noreply.github.com>
2019-09-18 21:36:25 +0800

    User

---
+++ Namespack/User/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"os/exec" // 执行shell命令
+	"syscall" // 调用底层操作系统
+	"os" // 标准输入输出错误
+	"log"  // 日志库
+	// 仅此介绍一遍
+	"os/user" // user库
+)
+
+func main() {
+	// fork出来的进程的初始化命令
+	cmd := exec.Command("sh")
+
+	// Namespace 的系统调用参数
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS | 
+			syscall.CLONE_NEWIPC |  // IPC 调用参数
+			syscall.CLONE_NEWPID |
+			syscall.CLONE_NEWNS | // 史上第一个 Namespace
+			syscall.CLONE_NEWUSER,
+	}
+
+	// 获取 mysql 用户 uid gid
+	user, err := user.Lookup("mysql")
+    if err == nil {
+       uid, _ := strconv.Atoi(user.Uid)  // 字符串转int
+       gid, _ := strconv.Atoi(user.Gid) 
+       cmd.SysProcAttr.Credential = &syscall.Credential{Uid: uint32(uid), Gid: uint32(gid)} // 以 mysql 用户执行 os.exec
+    }
+
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	
+	err := cmd.Run()
+
+	if err != nil {
+		// 等价于fmt.Println(); os.Exit(1);
+		log.Fatal(err)
+	}
+
+}
