99ef0489cad651a043d5ca8f386d1b951e5c34be
srstack <39378935+srstack@users.noreply.github.com>
2019-09-21 00:08:53 +0800

    cgroup

---
+++ Cgroup/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"fmt"
+	"os/exec"
+	"path"
+	"os"
+	"io/ioutil"
+	"syscall"
+	"strconv"
+)
+
+// 设置系统hierachy路径常量
+
+const cgroupHierarchyMemoryPath = "/sys/fs/cgroup/memory"
+
+
+func main() {
+	// fork出来的进程的初始化命令
+	cmd := exec.Command("sh")
+
+	// 获取当前用户
+
+	uid := syscall.Getuid()  // 字符串转int
+	gid := syscall.Getgid()
+
+	log.Printf("uid=%d,gid=%d", uid, gid)
+
+	// Namespace 的系统调用参数
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS | 
+			syscall.CLONE_NEWIPC |  // IPC 调用参数
+			syscall.CLONE_NEWPID |
+			syscall.CLONE_NEWNS | // 史上第一个 Namespace
+			syscall.CLONE_NEWUSER |
+			syscall.CLONE_NEWNET,
+		UidMappings: []syscall.SysProcIDMap{
+			{
+				ContainerID: 0, // 映射为root
+				HostID:      uid,
+				Size:        1,
+			},
+		},
+		GidMappings: []syscall.SysProcIDMap{
+			{
+				ContainerID: 0, // 映射为root
+				HostID:      gid,
+				Size:        1,
+			},
+		},
+	}
+	
+    // cmd.SysProcAttr.Credential = &syscall.Credential{Uid: uint32(uid), Gid: uint32(gid)} // 以 qsr 用户执行 os.exec
+
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	
+	err := cmd.Run()
+
+	if err != nil {
+		// 等价于fmt.Println(); os.Exit(1);
+		log.Fatal(err)
+	} else {
+		os.Mkdir(path.join(cgroupHierarchyMemoryPath, "testmemorylimit"),0755) // 创建cgroup
+
+		ioutil.WriteFile(
+			path.join(cgroupHierarchyMemoryPath, "testmemorylimit", "tasks"), // 将限制进程pid写入cgroup tasks中
+			[]byte(strconv.Itoa(cmd.Process.Pid)), // 字节
+			0644
+		)
+
+		ioutil.WriteFile(
+			path.join(cgroupHierarchyMemoryPath, "testmemorylimit", "memory.limit_in_bytes"),
+			[]byte("50m"), // 限制内存使用50M
+			0644
+		)
+	}
+}
