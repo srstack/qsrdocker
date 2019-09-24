2e4fe1cdbc7af3e4c6670bb772cf08563d7a8234
srstack <39378935+srstack@users.noreply.github.com>
2019-09-24 22:47:53 +0800

    Update main.go

--- main.go
+++ main.go
@@ -2,9 +2,49 @@ package main
 
 import (
 	"os"
+
 	log "github.com/Sirupsen/logrus"
-	"github.com/urfave/cil"
+	"github.com/urfave/cli"
 )
 
+const usage = ` qsrdocekr is a simple container runtime implementation.`
+
+var runCmd = cli.Command{}
+
+var initCmd = cli.Command{}
+
 func main() {
-	fmt.Println("ok")
+
+	/*
+		初始化cli
+	*/
+	qsrdocker := cli.NewApp()
+	qsrdocker.Name = "qsrdocker"
+	qsrdocker.Usage = usage
+
+	/*
+		定义cli的runCmd initCmd
+	*/
+	qsrdocker.Commands = []cli.Command{
+		initCmd,
+		runCmd,
+	}
+
+	/*
+		设定log配置项
+	*/
+	qsrdocker.Before = func(context *cli.Context) error {
+		//  log 使用 json 格式序列化
+		log.SetFormatter(&log.JSONFormatter{})
+		log.SetOutput(os.Stdout)
+
+		return nil
+	}
+
+	err := qsrdocker.Run(os.Args)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
