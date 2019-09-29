d7cbf7be459905d4984a9a9b9d5d777131a34724
srstack <39378935+srstack@users.noreply.github.com>
2019-09-30 01:32:11 +0800

    Update main.go

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ var runCmd = cli.Command{
 	Name: "run",
 	Usage: `Create a container with namespace and cgroup
 			qsrdocker run -ti [command]
-			-ti container's stdin stdout and stderr improt bash stdin stdout and stderr \n`,
+			-ti container's stdin stdout and stderr improt bash stdin stdout and stderr \r\n`,
 
 	Flags: []cli.Flag{
 		cli.BoolFlag{
@@ -49,7 +49,7 @@ init 初始化函数, 该函数/操作为 runCmd 默认会调用的内部方法�
 var initCmd = cli.Command{
 	Name: "init",
 	Usage: `init container process run user's process in container, Do not call it outside
-			warring: you can not use init in bash/sh \n`,
+			warring: you can not use init in bash/sh \r\n`,
 
 	/*
 		1. 获取传递过来的 参数
